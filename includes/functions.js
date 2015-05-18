84f3d6b76d9510004da368bb170cf17944d07da6
bill rowland <bill.rowland@bcbsnc.com>
2015-05-17 20:34:28 -0400

    buildManyFiles

--- includes/functions.js
+++ includes/functions.js
@@ -1,3 +1,4 @@
+var exports = module.exports = {};
 var Q = require('q');
 var prompt = require('prompt');
 var request = require("request");
@@ -5,12 +6,49 @@ var FS = require('fs');
  var path = require('path');
 var jade = require('jade');
 
-module.exports.getJson = function (fname){
+exports.buildManyFiles = function(values){
+	var deferred = Q.defer();
+
+	var builds = [];
+	var filesToBuild = ['test.jade', 'test.jade'];
+	var dir = 'views/';
+	for (var i = 0; i < filesToBuild.length; i++) {
+		builds.push(this.buildFile(dir + filesToBuild[i], values));
+	};
+
+	var bunchOPromises = Q.all(builds);
+
+	bunchOPromises
+	.then(function (results) {
+		deferred.resolve(results);
+	}).fail(function(e){
+		deferred.reject('buildManyFiles error: ', e);
+	});
+	return deferred.promise;
+};
+
+exports.buildFile = function(file, values){
+	var deferred = Q.defer();
+
+	FS.readFile(file, 'utf8', function (err, data) {
+		if (err){
+			deferred.reject('buildFile error: ' + err);
+		} else {
+			var options = { 'filename': path.join(__dirname, 'layout.jade'), 'pretty': '\t'}
+			var fn = jade.compile(data,options);
+			var html = fn(values);
+			deferred.resolve(html + 'XXXX:' + file);
+		}
+	});
+	return deferred.promise;
+}
+
+
+exports.getJson = function (fname){
 	var deferred = Q.defer();
 	request({url: fname}, function (err, response, result) {
 		if (!err && response.statusCode == 200) {
-			var schema  = JSON.parse(result);
-			deferred.resolve(schema);
+			deferred.resolve(result);
 		} else {
 			deferred.reject('getJson error: ',response.statusCode);
 		}
@@ -18,11 +56,12 @@ module.exports.getJson = function (fname){
 	return deferred.promise;
 };
 
-module.exports.getUserInput = function(schema){
+exports.getUserInput = function(schema){
 	var deferred = Q.defer();
 
 	prompt.delimiter = ":  ".green;
 	prompt.start();
+
 	prompt.get(schema, function (err, result) {
 		if (!err) {
 			deferred.resolve(result);
@@ -33,23 +72,7 @@ module.exports.getUserInput = function(schema){
 	return deferred.promise;
 }
 
-module.exports.buildFile = function(values){
-	var deferred = Q.defer();
-
-	FS.readFile('views/layout.jade', 'utf8', function (err, data) {
-		if (err){
-			deferred.reject('buildFile error: ' + err);
-		} else {
-			var options = { 'filename': path.join(__dirname, 'layout.jade'), 'pretty': '\t'}
-			var fn = jade.compile(data,options);
-			var html = fn(values);
-			deferred.resolve(html);
-		}
-	});
-	return deferred.promise;
-}
-
-module.exports.writeFile = function(html){
+exports.writeFile = function(html){
 	var deferred = Q.defer();
 	fname = 'test2.html';
 	FS.writeFile(fname, html, function (err) {
@@ -62,7 +85,20 @@ module.exports.writeFile = function(html){
 	return deferred.promise;
 }
 
-module.exports.exit = function(msg){
+exports.exit = function(msg){
 	console.log('\r\n', msg, '\r\n\r\n');
 	process.exit(code=0);
+}
+
+exports.readFile = function(fname){
+	var deferred = Q.defer();
+
+	FS.readFile(fname, 'utf8', function (err, contents) {
+		if (err){
+			deferred.reject('readFile error: ' + err);
+		} else {
+			deferred.resolve(eval(contents));
+		}
+	});
+	return deferred.promise;
 }
--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-var req = require('./includes/require.js');
 var FS = require('fs');
 var Q = require('q');
-var express = require("express");
 var func = require('./includes/functions.js');
 
-var schemaUrl = 'http://localhost/repos/buildIt/schema.json';
+var schemaUrl = 'http://localhost/repos/buildIt/schema.prompt';
+var schemaFile = 'schema.json';
 var tplDataUrl = 'http://localhost/repos/buildIt/tpl_data.json';
 
-func.getJson(schemaUrl)
+func.readFile(schemaFile)
 	.then(function(schema){
 		func.getUserInput(schema)
 		.then(function(results){
-			func.buildFile(results)
- 			.then(function(html){
- 				func.writeFile(html)
+			func.buildManyFiles(results)
+			.then(function(x){
+				console.log('xxxxxxxxxxxxxxxxxx - ',x.length);
+//			}).fail(function(e){ console.log('3',e) });
+			// func.buildFile(results)
+ 		// 	.then(function(html){
+// 				func.writeFile(html)
+ 				func.writeManyFiles(x)
  				.then(function(msg){
 					console.log(msg);
  					func.exit('finished\r\n');
  				}).fail(function(e){console.log('1',e) });
  			}).fail(function(e){ console.log('2',e) });
+
 		}).fail(function(e){ console.log('3',e) });
 	}).fail(function(e){ console.log('4',e) });
+
+
+/* files to build
+
+index.html
+/css/
+	normalize.css
+	main.css
+/js/
+	controllers.js
+	main.js
+o	db.js
+o	functions.js
+o	movieAPIs.js
+o	svc.js
+/views/
+	main.html
+o	restCalls.html
+
+*/
